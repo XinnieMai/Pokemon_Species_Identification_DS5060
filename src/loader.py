437b340a559517fd5059a20171ab302829e66a83
rtharrin <rtharrin@outlook.com>
2026-03-07 14:13:35 -0500

    starting loader, get download and dictionary, working on image loading next

--- src/loader.py
+++ src/loader.py
@@ -2,3 +2,22 @@
 # this is to create file structure
 
 # https://www.kaggle.com/datasets/vishalsubbiah/pokemon-images-and-types
+
+import kagglehub
+import pandas as pd
+
+
+#Download data via kagglehub if it doesn't already eist
+class Loader:
+
+    data_path = None
+    def download_data(self):
+        # Check if dataset is already downloaded
+        self.data_path = kagglehub.dataset_download("vishalsubbiah/pokemon-images-and-types")
+        print("Dataset downloaded to:", self.data_path)
+
+
+    def get_dictionary(self):
+       return pd.read_csv(self.data_path + "/pokemon.csv")
+    
+    def get_images(self):
