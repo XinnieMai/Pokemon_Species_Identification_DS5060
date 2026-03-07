38361e52d9c2d333609b813ea3eec7d1ab57b34e
rtharrin <rtharrin@outlook.com>
2026-03-07 13:40:37 -0500

    Added .gitignore,  license, and file structure.  Actual development will be done via feature branches from this point on my end

---
+++ src/loader.py
@@ -0,0 +1,4 @@
+# This is a placeholder, loader will be developed via a feature branch and then merged into main
+# this is to create file structure
+
+# https://www.kaggle.com/datasets/vishalsubbiah/pokemon-images-and-types
---
+++ tests/loader_test.py
@@ -0,0 +1,3 @@
+#Similar to loader this will be developed in a feature branch and merged into main, this is to create file structure
+#This will test to ensure that the loader is working correctly and can be used for regression testing in the future
+
