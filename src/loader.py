d9ec10bca03b91f0dbf257b2963240f86b5ad63a
rtharrin <rtharrin@outlook.com>
2026-04-25 10:41:43 -0400

    Delete src directory

--- src/loader.py
+++
@@ -1,4 +0,0 @@
-# This is a placeholder, loader will be developed via a feature branch and then merged into main
-# this is to create file structure
-
-# https://www.kaggle.com/datasets/vishalsubbiah/pokemon-images-and-types
